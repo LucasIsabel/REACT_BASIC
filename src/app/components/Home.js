4ab977eacc7c25ff233dcd44ba5492d255ea5337
Lucas Isabel <lucky.o.isa@gmail.com>
2016-11-09 14:52:55 -0200

    Porps

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,9 +2,20 @@ import React from "react";
 
 export class Home extends React.Component{
     render(){
+
+        let text = "My Component";
+
         return (
             <div>
-                <p> In a new Component !</p>
+                <p> {text} </p>
+                <p> Your name is {this.props.name}, yout age is {this.props.age}</p>
+                <p> User Object => Name : {this.props.user.name}</p>
+                <div>
+                    <h4>Hobbies</h4>
+                    <ul>
+                        {this.props.user.hobbies.map( (data,key) => <li key={key}> {data} </li>)}
+                    </ul>
+                </div>
             </div>
         );
     }
